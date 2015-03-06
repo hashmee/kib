f628bec8c63c0dbde409109aae544ed70bee9974
hashmee <hashmee.smf@gmail.com>
2015-03-06 23:47:19 +0530

    added precompile to false in production.rb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
 //= require twitter/bootstrap
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require jquery
 //= require jquery_ujs
