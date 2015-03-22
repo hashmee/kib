64791661a81ca69baa2c206a0fa0ef992a7d80b9
hashmee <hashmee.smf@gmail.com>
2015-03-22 15:11:02 +0530

    changed production.rb to avoid deprecation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
    config.cache_classes = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.assets.compress = true
   config.assets.compile = true
   config.assets.initialize_on_precompile = false
